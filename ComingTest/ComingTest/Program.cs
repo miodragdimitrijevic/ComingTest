683cebbd9373b8a20a4782f32cb498e21c1ee403
miodrag_dimitrijevic <miskoodm@elfak.rs>
2020-03-02 18:48:37 +0100

    Resenje kviza sa Job Fair-a

---
+++ ComingTest/ComingTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ComingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { -1, 1, 2, 4};
+            Solution solution = new Solution();
+            Console.WriteLine("Resenje prvog zadatka:");
+            solution.swapVariables(); //prvi zadatak;
+
+            Console.WriteLine("Resenje drugog zadtka: ");
+            //drugi zadatak;
+            int rezultatDrugiZadatak =solution.solution(A);
+            Console.WriteLine("Najmanja vrednost koja nije sadrzana u datom nizu A a veca je od nule je "
+                + rezultatDrugiZadatak);
+
+            Console.WriteLine("Resenje treceg zadatka:");
+            //treci zadatak
+           int faktorijel= solution.fact(5);
+            Console.WriteLine("Faktorijel zadatog broja je " + faktorijel);
+            
+          
+           
+            
+        }
+
+        
+    }
+}
---
+++ ComingTest/ComingTest/Solution.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComingTest
+{
+   public class Solution
+    {
+        public Solution()
+        {
+
+        }
+        //zadatak 1
+        public void swapVariables()
+        {
+            int a = 1;
+            int b = 5;
+            Console.WriteLine("Stara vrednost a je " + a + "a stara vrednost b je" + b);
+            a = a + b;
+            b = a - b;
+            a = a - b;
+            Console.WriteLine("Nova vrednost a je " + a + " a nova vrednost b je " + b);
+
+        }
+        //zadatak 2
+        public int solution(int[] A)
+        {
+            int minimum = min(A);
+            bool flag = false;
+            minimum++;
+            while (flag == false)
+            {
+                if (!isMemberofArray(minimum, A))
+                {
+                    flag = true;
+
+                }
+                else
+                {
+                    minimum++;
+
+                }
+            }
+            return minimum;
+        }
+        public int min(int[] A)
+        {
+            int minel = 1;
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] <= minel && A[i] > 0)
+                {
+                    minel = A[i];
+                }
+            }
+            return minel;
+        }
+
+        public bool isMemberofArray(int el, int[] A)
+        {
+
+            foreach (int e in A)
+            {
+                if (e == el)
+                    return true;
+            }
+            return false;
+        }
+        //zadatak 3
+        public int fact(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            return n * fact(n - 1);
+        }
+    }
+}
